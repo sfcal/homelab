8fcb55272d5584b53d63727e422d34b93d58d61f
Sam Calvert <git@samuelcalvert.com>
2025-03-20 11:19:02 -0400

    Update controlplane.tf to install tf

--- terraform/proxmox/controlplane.tf
+++ terraform/proxmox/controlplane.tf
@@ -114,7 +114,15 @@ resource "proxmox_vm_qemu" "controlplane" {
       "git --version"
     ]
   }
-
+  # Install Terraform
+  provisioner "remote-exec" {
+    inline = [
+      "curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg"
+      "echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list"
+      "sudo apt update"
+      "sudo apt install -y terraform"
+    ]
+  }
   # Clone the repository
   provisioner "remote-exec" {
     inline = [
