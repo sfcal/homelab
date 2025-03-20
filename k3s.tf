f43259c90f77a5c687b38cdc54384ea386169584
sfcal <git@samuelcalvert.com>
2025-03-20 15:00:39 +0000

    initial k3s tf

---
+++ k3s.tf
@@ -0,0 +1,163 @@
+resource "proxmox_vm_qemu" "k3s-master" {
+  
+  # -- General settings
+
+  name = "k3s-master-0${count.index + 1}"
+  desc = "description"
+  count = 3
+  agent = 0
+  target_node = "pve-prod01"
+  #vmid = "401"
+
+  clone = "ubuntu-server-noble"
+  #full_clone = true
+
+  # -- Boot Process
+
+  onboot = true 
+  automatic_reboot = true
+
+  # -- Hardware Settings
+
+  qemu_os = "other"
+  bios = "seabios"
+  cores = 2
+  sockets = 1
+  cpu_type = "host"
+  memory = 4096
+  
+
+  network {
+    id     = 0  # <-- ! required since 3.x.x
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  
+  scsihw = "virtio-scsi-single"  # <-- (Optional) Change the SCSI controller type, since Proxmox 7.3, virtio-scsi-single is the default one         
+  
+  disks {  # <-- ! changed in 3.x.x
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+    virtio {
+      virtio0 {
+        disk {
+          storage = "local-lvm"
+          size = "12G"  # <-- Change the desired disk size, ! since 3.x.x size change will trigger a disk resize
+          iothread = true  # <-- (Optional) Enable IOThread for better disk performance in virtio-scsi-single
+          replicate = false  # <-- (Optional) Enable for disk replication
+        }
+      }
+    }
+  }
+
+  # -- Cloud Init Settings
+  os_type = "cloud-init"
+  #ipconfig0 = "ip=10.1.10.40/24,gw=10.1.10.1"
+  ipconfig0 = "ip=dhcp"
+  nameserver = "8.8.8.8"  # <-- Change to your desired DNS server
+  ciuser = "sfcal"
+  sshkeys = var.ssh_public_key  # <-- (Optional) Change to your public SSH key
+
+
+ # Ensure that VM gets unique ID
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+#   # SSH connection for provisioning
+#   connection {
+#     type        = "ssh"
+#     host        = self.default_ipv4_address
+#     user        = "sfcal"
+#     password    = "Proxmox4me!"
+#     private_key = file("~/.ssh/id_ed25519")
+#     timeout     = "2m"
+#   }
+
+resource "proxmox_vm_qemu" "k3s-worker" {
+  
+  # -- General settings
+
+  name = "k3s-worker-0${count.index + 1}"
+  desc = "description"
+  count = 2
+  agent = 0
+  target_node = "pve-prod01"
+  #vmid = "401"
+
+  clone = "ubuntu-server-noble"
+  #full_clone = true
+
+  # -- Boot Process
+
+  onboot = true 
+  automatic_reboot = true
+
+  # -- Hardware Settings
+
+  qemu_os = "other"
+  bios = "seabios"
+  cores = 2
+  sockets = 1
+  cpu_type = "host"
+  memory = 4096
+  
+
+  network {
+    id     = 0  # <-- ! required since 3.x.x
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  
+  scsihw = "virtio-scsi-single"  # <-- (Optional) Change the SCSI controller type, since Proxmox 7.3, virtio-scsi-single is the default one         
+  
+  disks {  # <-- ! changed in 3.x.x
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+    virtio {
+      virtio0 {
+        disk {
+          storage = "local-lvm"
+          size = "12G"  # <-- Change the desired disk size, ! since 3.x.x size change will trigger a disk resize
+          iothread = true  # <-- (Optional) Enable IOThread for better disk performance in virtio-scsi-single
+          replicate = false  # <-- (Optional) Enable for disk replication
+        }
+      }
+    }
+  }
+
+  # -- Cloud Init Settings
+  os_type = "cloud-init"
+  #ipconfig0 = "ip=10.1.10.40/24,gw=10.1.10.1"
+  ipconfig0 = "ip=dhcp"
+  nameserver = "8.8.8.8"  # <-- Change to your desired DNS server
+  ciuser = "sfcal"
+  sshkeys = var.ssh_public_key  # <-- (Optional) Change to your public SSH key
+
+
+ # Ensure that VM gets unique ID
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+output "vm_ip" {
+  value = proxmox_vm_qemu.k3s-master.default_ipv4_address
+  value = proxmox_vm_qemu.k3s-worker.default_ipv4_address
+
+}
