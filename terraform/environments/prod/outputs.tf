c77e40c836069765c5f399559b0b11d256f1f621
sam calvert <desktop@samuelcalvert.com>
2025-06-20 22:54:10 -0400

    bad refs

--- terraform/environments/prod/outputs.tf
+++ terraform/environments/prod/outputs.tf
@@ -16,15 +16,20 @@ output "k3s_worker_ips" {
 
 output "dns_servers" {
   description = "DNS server information"
-  value       = module.dns_servers.dns_servers
+  value = {
+    vm_id       = module.dns_server.vm_id
+    vm_name     = module.dns_server.vm_name
+    ip_address  = module.dns_server.ip_address
+    proxmox_node = module.dns_server.proxmox_node
+  }
 }
 
 output "dns_server_ips" {
   description = "DNS server IP addresses"
   value = {
-    primary   = module.dns_servers.primary_dns_ip
-    secondary = module.dns_servers.secondary_dns_ip
-    all       = module.dns_servers.dns_server_ips
+    primary   = module.dns_server.ip_address
+    secondary = null
+    all       = [module.dns_server.ip_address]
   }
 }
 
@@ -32,6 +37,11 @@ output "infrastructure_summary" {
   description = "Summary of all infrastructure nodes"
   value = {
     k3s_nodes   = module.k3s_cluster.all_nodes
-    dns_servers = module.dns_servers.dns_servers
+    dns_servers = {
+      vm_id       = module.dns_server.vm_id
+      vm_name     = module.dns_server.vm_name
+      ip_address  = module.dns_server.ip_address
+      proxmox_node = module.dns_server.proxmox_node
+    }
   }
 }
